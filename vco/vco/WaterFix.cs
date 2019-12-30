0eaf868e9c5801e3b1ffa6ceac5c59ca972fdc39
Ryan Maybury <ryan2711@hotmail.co.uk>
2019-12-30 20:02:47 +0000

    Updated WaterFix.cs, now the water will only be changed if these conditions are met: VCO is loaded, if it is then it will check if the player is in the area where VCO is, if the player is, the water will be flattened. Otherwise it shall be reset and left.

--- vco/vco/WaterFix.cs
+++ vco/vco/WaterFix.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using GTA;
 using GTA.Native;
+using GTA.Math;
 
 namespace vco
 {
     class WaterFix : Script
     {
         float waveScale = 0.0f; // What should we set the wave scale to when VCO is loaded? 0.0 = an attempt for it to be flat pretty much. 
+        Vector3 vcoArea1 = new Vector3(-2822.374f, -4967.567f, 1.0f); // These vectors is a squared area/box of the VCO map
+        Vector3 vcoArea2 = new Vector3(-5502.316f, -546.8012f, 192.5952f); // These vectors is a squared area/box of the VCO map
 
         public WaterFix()
         {
@@ -21,7 +24,10 @@ namespace vco
         {
             if (UsefulFunctions.IsVCOLoaded())
             {
-                Function.Call(Hash.SET_DEEP_OCEAN_SCALER, waveScale); // Sets the wave scale to whatever the waveScale value is, only if VCO is loaded.
+                if (Function.Call<bool>(Hash.IS_ENTITY_IN_AREA, Game.Player, vcoArea1.X, vcoArea1.Y, vcoArea1.Z, vcoArea2.X, vcoArea2.Y, vcoArea2.Z)) // Checks if player is in the area before changing water.
+                {
+                    Function.Call(Hash.SET_DEEP_OCEAN_SCALER, waveScale); // Sets the wave scale to whatever the waveScale value is, only if VCO is loaded.
+                }
             }
             else
             {
