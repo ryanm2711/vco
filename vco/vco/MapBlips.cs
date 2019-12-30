a240d6ebbd97e9dff50267579aca104a411c5720
Ryan Maybury <ryan2711@hotmail.co.uk>
2019-12-30 21:35:14 +0000

    New cs file called MapBlips, created a blip system to make blips easily, functions are in UsefulFunctions and system is in MapBlips.cs

---
+++ vco/vco/MapBlips.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+
+namespace vco
+{
+    class MapBlips : Script
+    {
+        List<Blip> vcoMapBlips = new List<Blip>(); // We are creating a list so we can keep track of the blips on the player's map
+
+        bool hasCreatedBlips;
+        bool hasRemovedBlips;
+        public MapBlips()
+        {
+            Tick += OnTick;
+        }
+
+        static readonly Tuple<Vector3, string, int, bool, int>[] BlipsDefs = new Tuple<Vector3, string, int, bool, int>[]
+        {
+            Tuple.Create<Vector3, string, int, bool, int>(new Vector3(-3021.0f, -2565.0f, 0.0f), "Malibu Club", 614, true, 0), // Malibu Club
+        };
+
+        void OnTick(object sender, EventArgs e)
+        {
+            if (UsefulFunctions.IsVCOLoaded())
+            {
+                if (!hasCreatedBlips)
+                {
+                    foreach (Tuple<Vector3, string, int, bool, int> tuple in BlipsDefs) // Loops through our tuple and gets information to use in our custom create blip function
+                    {
+                        UsefulFunctions.CreateBlip(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, vcoMapBlips); // This is our custom function for creating blips, I recommend you use this for cleaner code :)
+                    }
+                    hasCreatedBlips = !hasCreatedBlips; // Reverses a boolean so it's = opposite
+                    hasRemovedBlips = !hasRemovedBlips;
+                }
+            }
+            else
+            {
+                if (!hasRemovedBlips)
+                {
+                    foreach (Blip b in vcoMapBlips) // Loops through the list of blips, b is the result of each blip
+                    {
+                        Function.Call(Hash.REMOVE_BLIP, b); // This removes every blip in list
+                    }
+                    vcoMapBlips.Clear(); // Clears our list so it's empty again
+
+                    hasRemovedBlips = !hasRemovedBlips; // Reverses a boolean so it's = opposite
+                    hasCreatedBlips = !hasCreatedBlips;
+                }
+            }
+        }
+    }
+}
--- vco/vco/UsefulFunctions.cs
+++ vco/vco/UsefulFunctions.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using GTA;
 using GTA.Native;
+using GTA.Math;
 
 namespace vco
 {
@@ -25,6 +26,30 @@ namespace vco
             {
                 return false;
             }
+        }
+
+        static void SetBlipName(Blip b, string name)
+        {
+            Function.Call(Hash.BEGIN_TEXT_COMMAND_SET_BLIP_NAME, "STRING");
+            Function.Call((Hash)0x6C188BE134E074AA, name); // Hash being used here is: ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME 
+            Function.Call(Hash.END_TEXT_COMMAND_SET_BLIP_NAME, b);
+        }
+
+        public static void CreateBlip(Vector3 pos, string blipName, int spriteID, bool shortRange, int colourID, List<Blip> list)
+        {
+            Blip b = World.CreateBlip(pos);
+            Function.Call(Hash.SET_BLIP_SPRITE, b, spriteID); // You can get it ID here: https://docs.fivem.net/docs/game-references/blips/
+            Function.Call(Hash.SET_BLIP_AS_SHORT_RANGE, shortRange); // True or false on whether the blips will appear on minimap when far away
+            Function.Call(Hash.SET_BLIP_COLOUR, b, colourID); // You can get the ID here: https://runtime.fivem.net/doc/natives/#_0x03D7FB09E75D6B7E 0 = white, most of the time you will be using that.
+            SetBlipName(b, blipName);
+            list.Add(b);
+        }
+
+        public static void TextBox(string text, bool looptext, bool beepsound, int duration)
+        {
+            Function.Call(Hash.BEGIN_TEXT_COMMAND_DISPLAY_HELP, "STRING");
+            Function.Call(Hash.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME, text);
+            Function.Call((Hash)0x238FFE5C7B0498A6, 0, looptext, beepsound, duration); // The hash being used here is: END_TEXT_COMMAND_DISPLAY_HELP
         }
     }
 }
