3693316924084299a0b866b005280cb7e6728c04
Ryan Maybury <ryan2711@hotmail.co.uk>
2019-12-30 18:55:41 +0000

    Created UsefulFunctions.cs file and added a IsVCOLoaded boolean function

---
+++ vco/vco/UsefulFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Native;
+using System.IO;
+
+namespace vco
+{
+    public class UsefulFunctions
+    {
+        static string[] vcoIPLS = File.ReadAllLines("scripts\\vco\\vco_ipls.txt");
+        public UsefulFunctions()
+        {
+
+        }
+
+        public static bool IsVCOLoaded()
+        {
+            foreach (string ipl in vcoIPLS)
+            {
+                if (Function.Call<bool>(Hash.IS_IPL_ACTIVE, ipl))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
